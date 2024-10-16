ddad572c175779d49b701db3cfeb16dfa8816076
Luann Mariano <luann.mariano2003@gmail.com>
2024-10-15 23:17:59 -0300

    comiiit

--- src/app/produtos/page.tsx
+++ src/app/produtos/page.tsx
@@ -1,9 +1,11 @@
 import Layout from '../layout';
+import Produtos from '../components/Produtos'
 
 export default function ProdutosPage() {
     return (
         <Layout>
         <h1>PRODUTOS</h1>
+        <Produtos />
         </Layout>
     );
 }
